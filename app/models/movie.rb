596574cc7753684806a0c70956a6b46c8ec22ea8
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-09 15:42:44 +0000

    added view templates, routes, and alerts

---
+++ app/models/movie.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  released    :boolean
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+
+end
